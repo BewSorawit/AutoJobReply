44e670bf5a18b1437729dda01d7e4ca0a53a40a7
Brown <sorawitchueachual70@gmail.com>
2024-09-05 20:18:33 +0700

    finish deploy mongo

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,6 @@ const Applicant = require("../models/Applicant");
 // Multer configuration for file upload
 const storage = multer.memoryStorage();
 
-// Define the destination for the CV files
-// const cvDestination = (req, file, cb) => {
-//     const userId = req.body.email.replace('@', '_').replace('.', '_'); // Use email as a user identifier
-//     const userDir = path.join(__dirname, `../public/users/${userId}`);
-
-//     // Create the user directory if it doesn't exist
-//     if (!fs.existsSync(userDir)) {
-//         fs.mkdirSync(userDir);
-//     }
-
-//     cb(null, userDir);
-// };
-
 const cvDestination = (req, file, cb) => {
   const userId = req.body.email.replace("@", "_").replace(".", "_");
   const userDir = path.join(__dirname, `../public/users/${userId}`);
